1fa91860ef2092fcddc8c6e40d0cf91bae6e42c9
Jason De Lorme <jasondel@microsoft.com>
2017-09-17 19:07:50 -0400

    Interating to generate dummy track events.

--- SlalomTrackerTest/CoursePassTest.cs
+++ SlalomTrackerTest/CoursePassTest.cs
@@ -12,13 +12,15 @@ namespace SlalomTracker
             Rope rope = new Rope(16);
             CoursePass pass = new CoursePass(CourseTest.CreateTestCourse(), rope);
 
-            pass.Track(DateTime.Now.Subtract(TimeSpan.FromSeconds(13)), 0.5, CourseTest.latitude, 
-                CourseTest.AddDistance(CourseTest.latitude, CourseTest.longitude, 10));
-
-            //foreach(var m in pass.Measurements)
-            //{
-            //    Console.WriteLine(m.HandlePosition);
-            //}
+            for (int i = 0; i < 30; i++)
+            {
+                pass.Track(DateTime.Now.Subtract(TimeSpan.FromSeconds(13)), 0.5, CourseTest.latitude,
+                    CourseTest.AddDistance(CourseTest.latitude, CourseTest.longitude, 10));
+            }
+            foreach (var m in pass.Measurements)
+            {
+                Console.WriteLine(m.HandlePosition);
+            }
         }
     }
 }
