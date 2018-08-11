86d60350f3e4d866527d049089f1cf85b0726030
Jason De Lorme <jjdelorme@yahoo.com>
2018-08-11 07:15:32 -0400

    Adding test for course features.

--- SlalomTrackerTest/CourseTest.cs
+++ SlalomTrackerTest/CourseTest.cs
@@ -8,6 +8,7 @@ namespace SlalomTracker
     {
         internal static double r_earth = 6378; // km
         internal static double latitude = 42.289087, longitude = -71.359124; // original lat/long position
+        private Course _course;
 
         /// <summary>
         /// Test hepler method that adds longitude distance to the start of the course.
@@ -38,11 +39,17 @@ namespace SlalomTracker
             return CreateTestCoursePass().Course;
         }
 
+        [TestInitialize]
+        public void Setup()
+        {
+            if (_course == null)
+                _course = CreateTestCourse();
+        }
+
         [TestMethod]
         public void TestGetCourseHeadingDeg()
         {
-            Course course = CreateTestCourse();
-            double heading = course.GetCourseHeadingDeg();
+            double heading = _course.GetCourseHeadingDeg();
             Assert.IsTrue((int)heading == 185, "heading is not equal");
         }
 
@@ -59,7 +66,12 @@ namespace SlalomTracker
         [TestMethod]
         public void TestGenerateCourseFeatures()
         {
-
+            _course.GenerateCourseFeatures();
+            // Quick spot check.
+            //Ball[5] = { X: 23,Y: 287}
+            //PreGate[3] = { X: 12.75,Y: 369}
+            Assert.IsTrue(_course.Balls[5].X == 23 && _course.Balls[5].Y == 287, "Balls are in wrong CoursePosition.");
+            Assert.IsTrue(_course.PreGates[3].X == 12.75 && _course.PreGates[3].Y == 369, "PreGates are in wrong CoursePosition.");
         }
     }
 }
