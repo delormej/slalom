8fcadbbd9e29725e5fe8944620d5fa2d0015597c
Jason De Lorme <jjdelorme@yahoo.com>
2018-08-31 08:14:23 -0400

    Refactoring entity class into its own file and removing CosmosDB reference as not necessary.

---
+++ Video/MetadataExtractor/SkiVideoEntity.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using Microsoft.WindowsAzure.Storage.Table;
+using SlalomTracker;
+using Newtonsoft.Json;
+
+namespace MetadataExtractor
+{
+    public class SkiVideoEntity : TableEntity
+    {
+        public SkiVideoEntity(string path, List<Measurement> measurements)
+        {
+            SetKeys(path);
+            this.Measurements = JsonConvert.SerializeObject(measurements);
+        }
+
+        public string Measurements { get; set; }
+
+        public string Skier { get; set; }
+
+        public double RopeLengthM { get; set; }
+
+        public double BoatSpeedMph { get; set; }
+
+        private void SetKeys(string path)
+        {
+            if (!path.Contains(Path.AltDirectorySeparatorChar))
+                throw new ApplicationException("path must contain <date>/Filename.");
+
+            int index = path.LastIndexOf(Path.AltDirectorySeparatorChar);
+            this.PartitionKey = path.Substring(0, index);
+            this.RowKey = path.Substring(index + 1, path.Length - index - 1);
+        }
+    }
+}
--- Video/MetadataExtractor/Storage.cs
+++ Video/MetadataExtractor/Storage.cs
@@ -7,32 +7,11 @@ using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Blob;
 using Microsoft.WindowsAzure.Storage.Table;
 using System.Collections.Generic;
-//using Newtonsoft.Json;
 using SlalomTracker;
 
 
 namespace MetadataExtractor
 {
-    public class SkiVideoEntity : TableEntity
-    {
-        public SkiVideoEntity(string path, List<Measurement> measurements)
-        {
-            SetKeys(path);
-        }
-
-        public List<Measurement> Measurements { get; set; }
-
-        private void SetKeys(string path)
-        {
-            if (!path.Contains(Path.AltDirectorySeparatorChar))
-                throw new ApplicationException("path must contain <date>/Filename.");
-
-            int index = path.LastIndexOf(Path.AltDirectorySeparatorChar);
-            this.PartitionKey = path.Substring(0, index);
-            this.RowKey = path.Substring(index+1, path.Length - index - 1);
-        }
-    }
-
     public class Storage
     {
         const string SKICONTAINER = "ski";
@@ -49,12 +28,9 @@ namespace MetadataExtractor
         public void AddMetadata(string path, List<Measurement> measurements)
         {
             SkiVideoEntity entity = new SkiVideoEntity(path, measurements);
-            //string json = JsonConvert.SerializeObject(measurements);
-            //Console.WriteLine(json);
-
             CloudTableClient client = _account.CreateCloudTableClient();
             CloudTable table = client.GetTableReference(SKITABLE);
-            TableOperation insert = TableOperation.Insert(entity);
+            TableOperation insert = TableOperation.InsertOrReplace(entity);
             Task createTask = table.CreateIfNotExistsAsync();
             createTask.Wait();
             Task insertTask = table.ExecuteAsync(insert);
