3f19ee332461476fa3639228ceeae81ec2f95685
Jason De Lorme <jason@insideride.com>
2018-10-15 07:39:11 -0400

    Work in progress on deploy/docker.

--- SkiConsole/deploy.sh
+++ SkiConsole/deploy.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+
+#
+# Need to build a container for the build with:
+#   shared volume
+#   dotnet sdk, build-essentials (gcc, make, etc...)
+#   clone the repo (or copy assets? )
+#   run the build
+#  
+# After this container completes, 
+# create another container with the resulting artifacts from the shared volume
+#
+
 # microsoft/dotnet:2.1-aspnetcore-runtime
 #deploy.sh
 cd ../../gpmf/demo/
@@ -7,4 +19,6 @@ mv ./gpmfdemo ../../slalom/SkiConsole/
 cd ../../slalom/SkiConsole/
 dotnet build ./SkiConsole.csproj
 dotnet publish 
-docker build -f Dockerfile ./bin/Debug/netcoreapp2.1/publish/
+# make a copy of the MP4 file for testing
+cp *.MP4 ./bin/Debug/netcoreapp2.1/publish/
+docker build -t skiconsole -f Dockerfile ./bin/Debug/netcoreapp2.1/publish/ 
