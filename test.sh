d07a6054ce636c80fb0e55bcaf037ea23eaca93b
Jason De Lorme <jjdelorme@yahoo.com>
2019-05-29 08:12:23 -0400

    Adding a test harness for http posting.

---
+++ test.sh
@@ -0,0 +1,26 @@
+#cat list.txt | jq  '.[]."videoUrl"'
+#curl -X POST -d "" "http://ski-app.azurewebsites.net/api/image?videoUrl=http://skivideostorage.blob.core.windows.net/ski/2018-06-08/GOPR0188.MP4" 
+while read p; do \
+  echo "$p"
+done <list2.txt
+
+
+cat list.txt | jq  '.[]."videoUrl"' | while read p; do \ 
+  curl -X POST -d "" "http://ski-app.azurewebsites.net/api/image?videoUrl=$p"
+
+cat list.txt | jq  '.[]."videoUrl"' | while read p; do \ 
+    echo `"$p"`
+done
+
+
+while IFS="" read -r p || [ -n "$p" ]
+do
+  printf '%ncurl -X POST -d "" "http://ski-app.azurewebsites.net/api/image?videoUrl=$p"' 
+done < list2.txt
+
+ cat list.txt | jq  '.[]."videoUrl"' -r | while read line ; do curl -X POST -d "" "http://ski-app.azurewebsites.net/api/image?videoUrl=$line" ; done
+
+
+ curl -X POST -d "" "http://ski-app.azurewebsites.net/api/image?videoUrl=http://skivideostorage.blob.core.windows.net/ski/2018-11-02/GOPR1024.MP4" 
+
+ ""
