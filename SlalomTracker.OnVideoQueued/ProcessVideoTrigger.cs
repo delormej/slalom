3fcd6d2a15d5c45736e4b3da9ab5889416f62cc3
Jason De Lorme <jjdelorme@yahoo.com>
2019-05-30 11:11:07 -0400

    Adding App Insights.

--- SlalomTracker.OnVideoQueued/ProcessVideoTrigger.cs
+++ SlalomTracker.OnVideoQueued/ProcessVideoTrigger.cs
@@ -7,24 +7,41 @@ using SlalomTracker;
 using SlalomTracker.Cloud;
 using MetadataExtractor;
 using Newtonsoft.Json;
+using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.ApplicationInsights.DataContracts;
 
 namespace SlalomTracker.OnVideoQueued
 {
-    public static class ProcessVideo
+    public class ProcessVideo
     {
         const string QueueName = "skiqueue";
         const string StorageConnection = "SKIBLOBS";
 
+        const string EVENT_FUNCTION_STARTED = "ProcessVideoTrigger_Started";
+        const string EVENT_VIDEO_DEQUEUED = "Video_Dequeued";
+        const string EVENT_VIDEO_PROCESSED = "Video_Processed";
+
+        private TelemetryClient insights;
+
+        public ProcessVideo(TelemetryConfiguration config)
+        {
+            this.insights = new TelemetryClient(config);
+            TrackEvent(EVENT_FUNCTION_STARTED, "");
+        }
+
         [FunctionName("ProcessVideoTrigger")]
-        public static void Run(
+        public void Run(
             [QueueTrigger(QueueName, Connection=StorageConnection)]string videoItem, ILogger log)
         {
+            TrackEvent(EVENT_VIDEO_DEQUEUED, videoItem);
             try 
             {
                 SkiVideoEntity video = JsonConvert.DeserializeObject<SkiVideoEntity>(videoItem);
                 log.LogInformation($"C# Queue trigger function processed: {videoItem}");
                 Console.WriteLine($"C# Queue trigger function processed: {video.Url}");
                 ProcessVideoMetadata(video.Url);
+                TrackEvent(EVENT_VIDEO_PROCESSED, video.Url);
                 Console.WriteLine("Succesfully processed: " + video.Url);
             }
             catch (Exception e)
@@ -41,6 +58,20 @@ namespace SlalomTracker.OnVideoQueued
             Storage storage = new Storage();
             string blobName = Storage.GetBlobName(localPath);
             storage.AddMetadata(blobName, json);
+        }
+
+        private void TrackEvent(string eventName, string value)
+        {
+            if (this.insights != null)
+            {
+                var evt = new EventTelemetry(eventName);
+                evt.Properties.Add("value", value);
+                this.insights.TrackEvent(evt);
+            }
+            else
+            {
+                Console.Error.WriteLine("Insights object not initialized, error trying to record: " + eventName);
+            }
         }
     }
 }
