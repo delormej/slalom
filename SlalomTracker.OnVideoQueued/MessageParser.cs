629a41aec1aca56345645c6fcae3f2fe0c3a2258
Jason De Lorme <jjdelorme@yahoo.com>
2019-05-31 07:44:09 -0400

    Updating to only parse 2 simple types, no need to parse google message.

--- SlalomTracker.OnVideoQueued/MessageParser.cs
+++ SlalomTracker.OnVideoQueued/MessageParser.cs
@@ -6,50 +6,26 @@ public class MessageParser
     public static string GetUrl(string message)
     {
         dynamic msg = JsonConvert.DeserializeObject(message);
-        if (msg.message != null)
-            return GetUrlFromGCloudEvent(message);
+        if (msg.Url != null)
+            return msg.Url;
         else if (msg.eventType == "Microsoft.Storage.BlobCreated")
-            return GetUrlFromAzureStorageEvent(message);
+            return GetUrlFromAzureStorageEvent(msg);
         else
             return null;
     }
 
-    private static string GetUrlFromGCloudEvent(string message)
+    private static string GetUrlFromAzureStorageEvent(dynamic msg)
     {
-        if (message == null) {
-            Console.Error.WriteLine("Cannot get Url from Event, message empty.");
-            return null;
-        }
-
-        dynamic objectEvent = JsonConvert.DeserializeObject(message);
-        string eventType = objectEvent.message.attributes.eventType;
-
-        if (eventType != "OBJECT_FINALIZE")
-        {
-            Console.Error.WriteLine("Invalid event type: " + eventType);
-            return null;
-        }
-
-        string bucketId = objectEvent.message.attributes.bucketId;
-        string objectId = objectEvent.message.attributes.objectId;
-        string data = objectEvent.message.data;
-
-        if (!objectId.ToUpper().EndsWith("MP4"))
-        {
-            Console.Error.WriteLine("Object was not an MP4 file: " + objectId);
-            return null;
-        }
-
-        string url = string.Format("https://storage.googleapis.com/{0}/{1}", bucketId, objectId);
-        return url;
-    }
-
-    private static string GetUrlFromAzureStorageEvent(string message)
-    {
-        dynamic msg = JsonConvert.DeserializeObject(message);
         if (msg.data != null && msg.data.url != null)
             return msg.data.url.ToString();
         else 
             return null;
     }
 }
+
+//{"Name":"2019-05-30/002.MP4","Url":"https://storage.googleapis.com/skivideos/2019-05-30/002.MP4"}
+//{"Name":"2019-05-21/005.MP4","Url":"https://storage.googleapis.com/skivideos/2019-05-21/005.MP4"}
+/*
+{"topic":"/subscriptions/40a293b5-bd26-47ef-acc3-c001a5bfce82/resourceGroups/ski/providers/Microsoft.Storage/storageAccounts/skivideostorage","subject":"/blobServices/default/containers/ski/blobs/2019-05-30/002.MP4","eventType":"Microsoft.Storage.BlobCreated","eventTime":"2019-05-31T11:10:24.7662457Z","id":"3ebcf7e8-d01e-0054-0da1-1786ab0645a4","data":{"api":"PutBlob","clientRequestId":"cd7c29e0-846e-4658-80bb-e62bd23fcb4a","requestId":"3ebcf7e8-d01e-0054-0da1-1786ab000000","eTag":"0x8D6E5B8947FD465","contentType":"application/octet-stream","contentLength":1018731,"blobType":"BlockBlob","url":"https://skivideostorage.blob.core.windows.net/ski/2019-05-30/002.json","sequencer":"00000000000000000000000000000FA40000000005c3e9f9","storageDiagnostics":{"batchId":"103b0961-bd62-417a-a0c4-562fdb69877c"}},"dataVersion":"","metadataVersion":"1"}
+
+ */
