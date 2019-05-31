25e59c1206af9bde17e52c37e90c541869e868ef
Jason De Lorme <jjdelorme@yahoo.com>
2019-05-31 07:45:35 -0400

    Refactoring tests.

--- SlalomTracker.Tests/RopeTest.cs
+++ SlalomTracker.Tests/RopeTest.cs
@@ -1,8 +1,8 @@
-using System;
+﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using SlalomTracker;
 
-namespace SlalomTracker.Tests 
+
+namespace SlalomTracker
 {
     [TestClass]
     public class RopeTest
@@ -20,5 +20,25 @@ namespace SlalomTracker.Tests
             double apexDeg2 = rope2.GetHandleApexDeg();
             Assert.IsTrue(Math.Round(apexDeg2, 2) == 28.57);
         }
-    }    
+
+        [TestMethod]
+        public void TestGetHandlePosition()
+        {
+            Rope rope = new Rope(23);
+
+            // Handle is on the right side.
+            var pos = rope.GetHandlePosition(65);
+
+            Assert.IsTrue(Math.Round(pos.X,4) == Math.Round(20.845079101842948, 4));
+            Assert.IsTrue(Math.Round(pos.Y,4) == Math.Round(9.7202200200360878, 4));
+
+            // When handle is past the pilon and on left side.
+            var pos2 = rope.GetHandlePosition(-91.2);
+
+            // X - 22.994955719921446 double
+            // Y - 0.48167565731721124    double
+            // Just ensure they are both negative.
+            Assert.IsTrue(pos2.X < 0 && pos2.Y < 0);
+        }
+    }
 }
