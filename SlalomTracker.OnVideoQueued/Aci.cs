dfabac954e65fed9be13455b3001f34394ca0e74
Jason De Lorme <jjdelorme@yahoo.com>
2019-06-06 10:27:07 -0400

    Test ACI code working for now.

--- SlalomTracker.OnVideoQueued/Aci.cs
+++ SlalomTracker.OnVideoQueued/Aci.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Net;
 using Microsoft.Azure.Management.Fluent;
@@ -16,88 +15,53 @@ namespace aci
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            // Build request to acquire managed identities for Azure resources token
-            //HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https://management.azure.com/");
-            //request.Headers["Metadata"] = "true";
-            //request.Method = "GET";
-
             try
             {
-                // Call /token endpoint
-                //HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-
-                // Pipe response Stream to a StreamReader, and extract access token
-               //StreamReader streamResponse = new StreamReader(response.GetResponseStream());
-                //string stringResponse = streamResponse.ReadToEnd();
-                //Console.WriteLine("Access: " + stringResponse);
-
                 string containerGroupName = "aci-msi-test";
                 string resourceGroupName = "ubuntu";
                 string containerImage = "busybox";
-                Dictionary<string, string> envVars = new Dictionary<string, string>();
-                //string[] startCommandLine = {"/bin/ash", "-c 'sleep 3600'"};
-                string startCommandLine = "/bin/ash -c sleep 3600";
+                string commandLineExe = "/bin/ash";
+                string[] commandLineArgs = new string[] {"-c", "sleep 3600"};
+                CreateContainerInstance(containerGroupName, 
+                    resourceGroupName, 
+                    containerImage,
+                    commandLineExe,
+                    commandLineArgs);
+            }
+            catch (Exception e)
+            {                               
+                string errorText = String.Format("{0} \n\n{1}", e.Message, e.InnerException != null ? e.InnerException.Message : "Unable to create container instance.");
+                Console.WriteLine("ERROR: " + e);
+            }
+        }
 
-                var msi = new MSILoginInformation(MSIResourceType.VirtualMachine); // try 1
-                var credentials = new AzureCredentials(msi, AzureEnvironment.AzureGlobalCloud);
-                var authenticated = Azure.Authenticate(credentials);
-                if (authenticated == null)
-                        throw new Exception("Unable to authenticate.");
-                IAzure azure = authenticated.WithDefaultSubscription();
+        static void CreateContainerInstance(string containerGroupName, 
+            string resourceGroupName, 
+            string containerImage,
+            string commandLineExe,
+            string[] commandLineArgs)
+        {
+            var msi = new MSILoginInformation(MSIResourceType.VirtualMachine); 
+            var credentials = new AzureCredentials(msi, AzureEnvironment.AzureGlobalCloud);
+            var authenticated = Azure.Authenticate(credentials);
+            IAzure azure = authenticated.WithDefaultSubscription();
 
-                IResourceGroup group = azure.ResourceGroups.GetByName(resourceGroupName);
-                var azureRegion = group.Region;
-/*
-    var containerGroup = azure.ContainerGroups.Define(containerGroupName)
-        .WithRegion(azureRegion)
-        .WithExistingResourceGroup(groupName)
-        .WithLinux()
-        .WithPublicImageRegistryOnly()
-        .WithoutVolume()
-        .DefineContainerInstance(containerGroupName + "-1")
-            .WithImage(containerImage)
-            .WithExternalTcpPort(80)
-            .WithCpuCoreCount(1.0)
-            .WithMemorySizeInGB(1)
-            .WithStartingCommandLines(startCommandLine.Split())
-            .WithEnvironmentVariables(envVars)
-            .Attach()
-        .WithDnsPrefix(containerGroupName)
-        .WithRestartPolicy(ContainerGroupRestartPolicy.Always)
-        .Create();
-*/
-
+            IResourceGroup group = azure.ResourceGroups.GetByName(resourceGroupName);
+            var azureRegion = group.Region;
             var containerGroup = azure.ContainerGroups.Define(containerGroupName)
                 .WithRegion(azureRegion)
                 .WithExistingResourceGroup(resourceGroupName)
                 .WithLinux()
-                    .WithPublicImageRegistryOnly()
+                .WithPublicImageRegistryOnly()
                 .WithoutVolume()
                 .DefineContainerInstance(containerGroupName + "-1")
                     .WithImage(containerImage)
                     .WithoutPorts()
                     .WithCpuCoreCount(1.0)
                     .WithMemorySizeInGB(1)
-                    //.WithStartingCommandLine(startCommandLine)
-                    .WithStartingCommandLine("/bin/ash", new string[] {"-c", "sleep 3600"})
-                    .WithEnvironmentVariables(envVars)
+                    .WithStartingCommandLine(commandLineExe, commandLineArgs)
                     .Attach()
-                //.WithDnsPrefix(containerGroupName)
                 .Create();
-
-                //Console.WriteLine($"Logs for container '{containerGroupName}-1':");
-                //Console.WriteLine(containerGroup.GetLogContent(containerGroupName + "-1"));
-                Console.WriteLine("Created...");
-
-
-            }
-            catch (Exception e)
-            {                               
-                string errorText = String.Format("{0} \n\n{1}", e.Message, e.InnerException != null ? e.InnerException.Message : "Acquire token failed");
-                Console.WriteLine("ERROR: " + e);
-            }
-
         }
     }
 }
