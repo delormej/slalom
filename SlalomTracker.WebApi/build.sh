008194fe6d357362f09120c2a8e5215bece096ed
Jason De Lorme <jjdelorme@yahoo.com>
2019-10-03 17:14:05 -0400

    Fixing build script and updating versions.

--- SlalomTracker.WebApi/build.sh
+++ SlalomTracker.WebApi/build.sh
@@ -1,2 +1,2 @@
 # This needs to be run from source root (down a directory)
-docker build -t skiwebapi:v6.0 -f ./SlalomTracker.WebApi/Dockerfile . && docker run --rm --name ski-dbg -p 80:80 -it -e SKIBLOBS=$SKIBLOBS skiwebapi:v6.0
+docker build -t skiwebapi:v6.0 -f ./SlalomTracker.WebApi/Dockerfile . && docker run --rm --name ski-dbg -p 80:80 -it -e SKIBLOBS=\"$SKIBLOBS\" skiwebapi:v6.0
