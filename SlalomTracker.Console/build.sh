0d7b5719fd22175dc528f2a9779a4232e5566e06
Jason De Lorme <jasondel@microsoft.com>
2019-10-20 19:14:54 -0400

    Adding option to build script to process a specific video.

--- SlalomTracker.Console/build.sh
+++ SlalomTracker.Console/build.sh
@@ -1,4 +1,16 @@
 : ${SKIBLOBS?"Need to set SKIBLOBS env variable."}
+
+# If an argument is passed, use it as a video.
+if [ $# -eq 0 ]
+  then
+    echo "No arguments supplied"
+    PROCESS_ARG=/bin/bash
+  else
+    PROCESS_ARG="./ski -p $1"
+fi
+
+echo $PROCESS_ARG
+
 #
 # This file used for DEBUG build
 #
@@ -10,4 +22,4 @@ docker build -t skiconsole -f ./SlalomTracker.Console/debug.Dockerfile .
 # Need to input env variable value here:
 #SKIBLOBS=
 # Ensure that $SKIBLOBS as an env variable has leading and trailing quotes ("")
-docker run -it -v$PWD:/data --rm -e SKIBLOBS="$SKIBLOBS" --name ski-dbg skiconsole /bin/bash
+docker run -it -v$PWD:/data --rm -e SKIBLOBS="$SKIBLOBS" --name ski-dbg skiconsole $PROCESS_ARG
