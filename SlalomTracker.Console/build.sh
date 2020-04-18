51c7eab4c9566563fd0d2765d1ee7efa0b4184a0
Jason De Lorme <jjdelorme@yahoo.com>
2020-04-18 16:41:26 -0400

    Tweaks to build script... skiblobs still not getting passed in.

--- SlalomTracker.Console/build.sh
+++ SlalomTracker.Console/build.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
 source prebuild.sh
 echo "github_token::$GITHUB_TOKEN"
+
+VERSION=v4.15.1
 #
 # Build .debug container
 #
 echo "Building DEBUG container."
-docker build -t skiconsole:debug --build-arg GITHUB_TOKEN=$GITHUB_TOKEN \
+docker build -t skiconsole:$VERSION --build-arg GITHUB_TOKEN=$GITHUB_TOKEN \
     -f ./SlalomTracker.Console/Dockerfile .
+#
 # To just use the debug image add --target build to the above and it won't build the release stage.
+#
 
 #
-# Launch debug container
+# Launch container
 #
-# Mounting volume is no longer working? -v$PWD:/data 
-docker run -it --rm -e SKIBLOBS="$SKIBLOBS" \
-    -e GOOGLE_APPLICATION_CREDENTIALS="/ski/gcloud.json" --name ski-dbg skiconsole:debug $PROCESS_ARG
+# env variable skiblobs still not working...
+docker run -it --rm \
+    -v $PWD:/shared \
+    -e SKIBLOBS="$SKIBLOBS" \
+    -e GOOGLE_APPLICATION_CREDENTIALS="/ski/gcloud.json" \
+    --name ski-dbg \
+    skiconsole:$VERSION 
+
+docker tag skiconsole:$VERSION wthacr.azurecr.io/skiconsole:$VERSION
+docker push wthacr.azurecr.io/skiconsole:$VERSION
