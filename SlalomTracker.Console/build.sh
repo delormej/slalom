0d326bd8fd22f5075e308e51cd93c3659819aeb7
Jason De Lorme <jjdelorme@yahoo.com>
2020-04-18 16:15:30 -0400

    Updating dockerfile for 3.1

--- SlalomTracker.Console/build.sh
+++ SlalomTracker.Console/build.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 source prebuild.sh
-
+echo "github_token::$GITHUB_TOKEN"
 #
 # Build .debug container
 #
 echo "Building DEBUG container."
-docker build -t skiconsole -f ./SlalomTracker.Console/debug.Dockerfile .
+docker build -t skiconsole:debug --build-arg GITHUB_TOKEN=$GITHUB_TOKEN \
+    -f ./SlalomTracker.Console/Dockerfile .
 
 #
 # Launch debug container
 #
 # Mounting volume is no longer working? -v$PWD:/data 
 docker run -it --rm -e SKIBLOBS="$SKIBLOBS" \
-    -e GOOGLE_APPLICATION_CREDENTIALS="/ski/gcloud.json" --name ski-dbg skiconsole $PROCESS_ARG
+    -e GOOGLE_APPLICATION_CREDENTIALS="/ski/gcloud.json" --name ski-dbg skiconsole:debug $PROCESS_ARG
