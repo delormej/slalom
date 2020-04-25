50878b3ea4c81eab3b6c25d738d1ed59e3f497c2
Jason De Lorme <jjdelorme@yahoo.com>
2020-04-24 21:05:38 -0400

    Adding web api for creating vtt file.

---
+++ SlalomTracker.WebApi/Controllers/VttController.cs
@@ -0,0 +1,46 @@
+using System;
+using SlalomTracker.Cloud;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+
+namespace SlalomTracker.WebApi.Controllers
+{
+    [ApiController]
+    public class VttController : Controller
+    {
+        ILogger<VttController> _logger;
+        private readonly IConfiguration _config;
+
+        public VttController(ILogger<VttController> logger, IConfiguration config)
+        {
+            _logger = logger;
+            _config = config;
+        }
+
+        [HttpGet]
+        [Route("api/vtt")]
+        public IActionResult Get(string recordedDate, string mp4Filename)
+        {
+            string vttContent;
+            try
+            {
+                Storage storage = new Storage();
+                SkiVideoEntity entity = storage.GetSkiVideoEntity(recordedDate, mp4Filename);
+                if (entity == null)
+                    throw new ApplicationException($"Unable to load SkiVideo {recordedDate}, {mp4Filename}");
+                WebVtt vtt = new WebVtt(entity);
+                vttContent = vtt.Create();
+                
+                _logger.LogInformation($"Created WebVtt for {recordedDate}, {mp4Filename}");
+
+                return Content(vttContent);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError("Unable to create WebVtt: " + e.Message);
+                return StatusCode(500);
+            }
+        }      
+    }
+}
