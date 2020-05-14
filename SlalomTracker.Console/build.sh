a48a15cb2ba20dce0419adb83087824b230f66e1
Jason De Lorme <jjdelorme@yahoo.com>
2020-05-13 21:25:11 -0400

    Reverting build script to release mode.

--- SlalomTracker.Console/build.sh
+++ SlalomTracker.Console/build.sh
@@ -21,7 +21,6 @@ echo "Building container."
 docker build -t $container --build-arg GITHUB_TOKEN=$GITHUB_TOKEN \
     --build-arg VERSION=$VERSION \
     --force-rm \
-    --target build \
     -f ./SlalomTracker.Console/Dockerfile .
 #
 # To just use the debug image add --target build to the above and it won't build the release stage.
@@ -40,9 +39,9 @@ docker run -it --rm \
     --cpus="2.0" \
     $container
 
-# az acr login -n wthacr
-# docker tag $container wthacr.azurecr.io/$container
-# docker push wthacr.azurecr.io/$container
+az acr login -n wthacr
+docker tag $container wthacr.azurecr.io/$container
+docker push wthacr.azurecr.io/$container
 
 #
 # Script to get message counts from Service Bus
