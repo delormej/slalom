d1e51482e7f2f6378798573e227a9ae6514e7382
delormej <jasondel@google.com>
2021-05-04 21:10:12 -0400

    Using project name.

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 # Builds the base docker image used for all builds & debug.
-REGISTRY=gcr.io/gke-ski
+REGISTRY=gcr.io/$GOOGLE_PROJECT_ID
 BUILDER_VERSION=2.0
 
 docker build --force-rm --no-cache -t $REGISTRY/skibuild:$BUILDER_VERSION \
